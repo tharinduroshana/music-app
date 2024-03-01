13d2e53513c7bbbf7276ad17a9976847ceea93fe
Tharindu Ranaweera <ranaweerat@gmail.com>
2024-03-01 21:51:24 +0200

    Added rules and messages

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,4 +1,10 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure
+} from 'vee-validate'
 import {
   required,
   min,
@@ -18,13 +24,39 @@ export default {
     app.component('ErrorMessage', ErrorMessage)
 
     defineRule('required', required)
+    defineRule('tos', required)
     defineRule('min', min)
     defineRule('max', max)
     defineRule('email', email)
     defineRule('min_value', minVal)
     defineRule('max_value', maxVal)
-    defineRule('confirmed', confirmed)
+    defineRule('password_mismatch', confirmed)
     defineRule('exclude', excluded)
+    defineRule('country_exclude', excluded)
     defineRule('alpha_spaces', alphaSpaces)
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required`,
+          min: `The field ${ctx.field} is too short`,
+          max: `The field ${ctx.field} is too long`,
+          alpha_spaces: `the field ${ctx.field} may only contain alphabetical characters and spaces.`,
+          email: `The field ${ctx.field} must be a valid email`,
+          min_value: `The field ${ctx.field} is too low`,
+          max_value: `The field ${ctx.field} is too high`,
+          exclude: `You are not allowed to use this value for the field ${ctx.field}`,
+          country_exclude: `Due to restrictions, we do not accept users from this location`,
+          password_mismatch: `The passwords do not match`,
+          tos: `You must accept the Terms of Service`
+        }
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid`
+
+        return message
+      }
+    })
   }
 }
